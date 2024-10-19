9533dd508c38427ea7a2a6f0ba387435e4c5526e
ni.palmam <ni.palmam@duocuc.cl>
2024-10-19 14:42:32 +0000

    asd

--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,37 @@
-import { Text, View } from "react-native";
+import { ImageBackground, Text, View } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { Link } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { CustomButton } from "../components/CustomButton";
+import { ScrollView } from "react-native";
 
 export default function Index() {
   return (
-    <View className="text-3xl items-center text-center justify-center flex-1"
-    >
-      <Text className="text-3xl font-pblack">Edit app/index.tsx to que bueno this screen.</Text>
-      <StatusBar />
-      <Link href="/Home" className="text-3xl">Ir a perfil</Link>
-    </View>
+    <SafeAreaView className="h-full w-full bg-white">
+      <ScrollView contentContainerStyle={{height: "100%"}}>
+        <View className="text-3xl items-center text-center justify-center flex-1">
+          <ImageBackground
+            source={require("../assets/images/earth alert logo (1).jpg")}
+            className="h-full w-full"
+          >
+            <Text className="text-black text-center text-2xl font-pbold bottom-0 absolute  ">
+              Registrarse
+            </Text>
+            <Text className="text-black text-center align-top text-2xl font-pbold"> 
+              Earth Alert
+            </Text>
+            
+    
+            <Link
+              href="/Home"
+              className="text-black text-right absolute right-0 bottom-0 text-2xl font-pbold"
+            >
+              Iniciar Sesión
+            </Link>
+          </ImageBackground>
+        </View>
+      <StatusBar backgroundColor="white" barStyle="light-content" />
+      </ScrollView>
+    </SafeAreaView>
   );
 }
